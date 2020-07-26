27f40f9d914bd986dee5a90844cb367581a96425
Omar Abdulaziz <omarabdu@uvic.ca>
2020-07-25 20:34:17 -0700

    Added Input File Handeling

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-int main(){
+FILE *fp;
+
+int main(int argc, char** argv){
+    if (argc <2){
+        perror("\nPlease input a .wav file\n");
+            return printf("\nPlease input a .wav file\n");
+    }
+    printf("\nInput .wav Filename:\t\t%s\n", argv[1]);
+    fp = fopen(argv[1], "rb");
+    if (fp ==NULL){
+        printf("Error opening .wav file %s", argv[1]);
+    }
     printf("Hello, World!");
     return 0;
 }
