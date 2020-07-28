19f3c7f8bf9536f479bfb4f03c3e7415567fce6d
Omar Abdulaziz <omarabdu@uvic.ca>
2020-07-27 20:38:44 -0700

    Added Functions to read wave file chunks

--- main.c
+++ main.c
@@ -15,6 +15,55 @@ int main(int argc, char** argv){
     if (fp ==NULL){
         printf("Error opening .wav file %s", argv[1]);
     }
-    printf("Hello, World!");
+    
     return 0;
+}
+
+void readWaveFileHeaders(){
+    printf("\nReading Wave Headers:\t...\n");
+    
+    //read wave header
+    fread(wave.waveHeader.sGroup,           sizeof(wave.waveHeader.sGroupID), 1, fp);
+    fread(buffer,                           sizeof(buffer),1 ,fp);
+    wave.waveHeader.dwFileLength = buffer[0] | buffer[1] <<8 |buffer[2] <<16 | buffer[3] <<24;
+    fread(wave.waveHeader.sRiffType,        sizeof(wave.waveHeader.sRiffType), 1, fp);
+
+    //read wave format chunk
+    fread(wave.waveFormatChunk.sGroupID,    sizeof(wave.waveFormatChunk.sGroupID), 1, fp);
+    fread(buffer,                           sizeof(buffer),1 ,fp);
+    wave.waveFormatChunk.dwChunkSize = buffer[0] | buffer[1] <<8 | buffer[2] <<16 | buffer[3] <<24;
+    fread(buffer,                           sizeof(__uint16_t), 1, fp);
+    wave.waveFormatChunk.wFormatTag = buffer[0] | buffer[1] << ;
+
+    fread(buffer,                           sizeof(__uint16_t), 1, fp);
+    wave.waveFormatChunk.wChannels = buffer[0] | buffer[1] <<8;
+
+    fread(buffer,                           sizeof(buffer), 1, fp);
+    wave.waveFormatChunk.dwSamplesPerSec = buffer[0] | buffer[1] << 8 | buffer[2] << 16 | buffer[3] << 24;
+
+    fread(buffer,                            sizeof(buffer), 1, fp);
+    wave.waveFormatChunk.dwAvgBytesPerSec = buffer[0] | buffer[1] <<8 | buffer[2] <<16 | buffer[3] <<24;
+    
+    fread(buffer,                            sizeof(__uint16_t), 1, fp);
+    wave.waveFormatChunk.wBlockAlign = buffer[0] | buffer[1] <<8;
+    
+    fread(buffer,                             sizeof(__uint16_t), 1, fp);
+    wave.waveFormatChunk.dwBitsPerSample = buffer[0] | buffer[1] <<8;
+
+    //read wave data chunk
+    fread(budffer, sizeof(buffer), 1, fp);
+    int notData = strcmp(buffer, "data");
+    int fileEnd = wave.waveHeader.dwFileLength - 40;
+    if (notData){
+        fread(buffer, sizeof(buffer), 1, fp);
+        notData = strcmp(buffer, "data");
+        if (!notData){
+            break;
+        }
+        fseek(fp, -3, SEEK_CUR);
+    }
+    strcpy(wave.waveDataChunk.sGroupID, buffer);
+    fread(buffer,                               sizeof(buffer), 1, fp);
+    wave.waveDataChunk.dwChunkSize = buffer[0] | buffer[1] << 8 | buffer[2] << 16 | buffer[3] << 24;
+    printtf("Reading Wave Headers:\t\tCompleted!\n\n");
 }
---
+++ main.h
@@ -0,0 +1,58 @@
+/**
+ *  @sGroupID:     type of file format: RIFF - generic container format
+ *                , stores data in tagged chunks
+ *  @dwFileLength: file size in bytes, minus 8 bits (ignores RIFF & WAVE tags)
+ *  @sRiffType:    to specify Riff file type, always "WAVE"
+ */
+struct WAVE_HEADER{
+    unsigned char sGroupID; 
+    __uint32_t dwFileLength; 
+    unisgned char sRiffType[4]; 
+};
+
+/*
+ *  @sGroupID:          Indicates format chunk of the file
+ *  @dwChunkSize:       The length of the chunk, minus sGroupID and dwChunkSize
+ *  @wFormatTag:        For wav files, this is always 0x1 and indicates PCM format (0x3 = IEEE FLoat, 0x6 = A-law, 0x7 = mu-law)
+ *  @wChannels:         Indicates the number of channels in the audio (0x1 = mono, 0x2 = stereo, etc.)
+ *  @dwSamplesPerSec:   The sampling rate (444100 for CDs, 48000 for DATs, etc.)
+ *  @dwAvgBytesPerSec:   The number of multichannel audio frames/sec - used to estimate how much memory will be needed
+ *  @wBlockAlign:       The number of bytes per multichannel audio fram
+ *  @dwBitsPerSample:   Bit depth (bits/sample) for the audio (8,16, 0r 32)
+ */
+struct WAVE_FORMAT_CHUNK{
+    unsigned char sGroupID[4];      //always "fmt " (dont forget space at end)
+    __uint32_t dwChunkSize;         //chunk size varies
+    __uint8_t wFormatTag;           // = 1
+    __uint16_t wChannels;           // = 1
+    __uint32_t dwSamplesPerSec;     //number of samples/sec varies
+    __uint32_t dwAvgBytesPerSec;    //= sample_rate * blockAlign
+    __uint16_t wBlockAlign;         // = wChannels * (dwBitsPerSample/8)
+    __uint16_t dwBitsPerSample;     // number of bits/sample varies
+};
+
+/*
+ *  @sGroupID:      Indicates next data chunk
+ *  @dwChunkSize:   Length of this array
+ *  @sampleData:    The Data stored in this array
+ */
+struct WAVE_DATA_CHUNK {
+    unsigned char sGroupID[4];  // "data"'
+    __uint32_t dwChunkSize;     //chunk size caries
+    short *sampleData;          // = dwSamplesPerSec * wChannels
+}
+
+struct WAVE {
+    struct WAVE_HEADER          waveHeader;
+    struct WAVE_FORMAT_CHUNK    waveFormatChunk;
+    struct WAVE_DATA_CHUNK      waveDataChunk;
+}
+
+/*
+ *FUNCTIONS:
+ */
+
+//read the original wave file
+void read_wave_file_headers();
+void read_wave_file_data_Samples();
+void read_wave_file();
